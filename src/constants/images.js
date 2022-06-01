0e7f4f7778083016249927762bd28fac59d14370
hazemEmad <hazem.emad@moweex.com>
2022-06-01 16:12:05 +0200

    init project

---
+++ src/constants/images.js
@@ -0,0 +1,6 @@
+const path = '../assets/images';
+
+export default IMAGES = {
+  POSTER: require(path + '/poster.jpeg'),
+  QUESTION_MARK: require(path + '/questionMark.png'),
+};
---
+++ src/screens/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}></View>
+  );
+};
+
+export default Home;
